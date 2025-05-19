d41e7ac4037f27d18463d794e9ee348c26a87ac0
Romuald la roulade <romuald.aversenq@gmail.com>
2025-05-19 12:03:03 +0200

    Simple mvvm setup

---
+++ V2_WPF_EasySave/V2_WPF_EasySave/App.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows;
+
+namespace V2_WPF_EasySave;
+
+public partial class App : Window
+{
+    public App()
+    {
+    }
+}
---
+++ V2_WPF_EasySave/V2_WPF_EasySave/ViewModel/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.ObjectModel;
+using System.IO;
+
+namespace V2_WPF_EasySave.ViewModel
+{
+    public class MainViewModel
+    {
+        public ObservableCollection<string> SavedJobs { get; set; }
+
+        private readonly string _jobDirectory = Path.Combine("..", "..", "..", "SavedJobs");
+
+        public MainViewModel()
+        {
+            SavedJobs = new ObservableCollection<string>();
+            LoadSavedJobs();
+        }
+
+        private void LoadSavedJobs()
+        {
+            if (!Directory.Exists(_jobDirectory))
+                Directory.CreateDirectory(_jobDirectory);
+
+            var files = Directory.GetFiles(_jobDirectory, "*.json");
+            foreach (var file in files)
+            {
+                SavedJobs.Add(Path.GetFileNameWithoutExtension(file));
+            }
+        }
+    }
+}

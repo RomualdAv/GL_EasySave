a5a94a739fa1e33a6aa785eb7491098b0035a52d
Romuald la roulade <romuald.aversenq@gmail.com>
2025-05-21 11:17:52 +0200

    vf translating system

---
+++ V2_WPF_EasySave/V2_WPF_EasySave/Utils/TranslateExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Markup;
+using System.Windows.Data;
+using System.Windows;
+
+namespace V2_WPF_EasySave.Utils
+{
+    public class TranslateExtension : MarkupExtension // Markup extension that generates a binding to languagemanager
+    {
+        public string Key { get; set; }
+
+        public TranslateExtension(string key)
+        {
+            Key = key;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            var binding = new Binding($"[{Key}]")
+            {
+                Source = LanguageManager.Instance,
+                Mode = BindingMode.OneWay
+            };
+            return binding.ProvideValue(serviceProvider);
+        }
+    }
+}
--- V2_WPF_EasySave/V2_WPF_EasySave/View/MainWindow.xaml.cs
+++ V2_WPF_EasySave/V2_WPF_EasySave/View/MainWindow.xaml.cs
@@ -1,11 +1,23 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using V2_WPF_EasySave.Utils;
 
-namespace V2_WPF_EasySave;
-
+namespace V2_WPF_EasySave.View;
 public partial class MainWindow : Window
 {
     public MainWindow()
     {
+        InitializeComponent();
         
+        LanguageManager.Instance.LoadLanguage("en");
+    }
+    
+    private void OnLanguageChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (sender is ComboBox comboBox && comboBox.SelectedItem is ComboBoxItem selectedItem)
+        {
+            string selectedLang = selectedItem.Content.ToString() ?? "fr";
+            LanguageManager.Instance.LoadLanguage(selectedLang);
+        }
     }
 }

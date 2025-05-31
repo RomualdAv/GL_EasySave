7abcc00248e1b3cffc8fe548d34536fb3edee13c
Romuald la roulade <romuald.aversenq@gmail.com>
2025-05-31 09:39:34 +0200

    Size Limit system

---
+++ V2_WPF_EasySave/V2_WPF_EasySave/View/SizeLimit.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using V2_WPF_EasySave.Utils;
+
+namespace V2_WPF_EasySave.View;
+
+public partial class SizeLimitWindow : Window
+{
+    private SizeLimit _settings;
+
+    public SizeLimitWindow()
+    {
+        InitializeComponent();
+        _settings = SizeLimit.Load();
+        MaxSizeBox.Text = _settings.MaxParallelFileSizeKB.ToString();
+    }
+
+    private void OnSaveClick(object sender, RoutedEventArgs e)
+    {
+        if (long.TryParse(MaxSizeBox.Text, out long value) && value > 0)
+        {
+            _settings.MaxParallelFileSizeKB = value;
+            _settings.Save();
+            MessageBox.Show("Paramètre sauvegardé.", "Succès", MessageBoxButton.OK, MessageBoxImage.Information);
+            Close();
+        }
+        else
+        {
+            MessageBox.Show("Veuillez entrer une valeur numérique valide.", "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
